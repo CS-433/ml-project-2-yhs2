6742173f6e714bac35d3325a953a56558495f0c1
NovemberDays <qkraudgjsqkqh@gmail.com>
2024-12-05 19:42:46 +0900

    edited Glove template scripts

--- Glove/build_vocab.sh
+++ Glove/build_vocab.sh
@@ -3,4 +3,5 @@
 # Note that this script uses GNU-style sed as gsed. On Mac OS, you are required to first https://brew.sh/
 #    brew install gnu-sed
 # on linux, use sed instead of gsed in the command below:
-cat twitter-datasets/train_pos_full.txt twitter-datasets/train_neg_full.txt | gsed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab_full.txt
+# Using sed instead of gsed since we are using Linux
+cat ../Data/twitter-datasets/train_pos_full.txt ../Data/twitter-datasets/train_neg_full.txt | sed "s/ /\n/g" | grep -v "^\s*$" | sort | uniq -c > vocab_full.txt
