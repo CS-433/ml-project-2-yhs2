48d593b3108fb8df26767d9af084f5da711a5992
agbld <agiblida@gmail.com>
2024-12-19 18:13:26 +0800

    Update README and add some comment on model.py, run.py

--- BERT-based/run.py
+++ BERT-based/run.py
@@ -8,12 +8,13 @@
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
 #
+# This code is based on the script https://github.com/huggingface/transformers/tree/main/examples/pytorch/text-classification
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" Finetuning the library models for sequence classification on ESUN Paraphrase Project."""
+""" Finetuning the library models for sequence classification."""
 # You can also adapt this script on your own text classification task. Pointers for this are left as comments.
 # %%
 import logging
